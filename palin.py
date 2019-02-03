2d8ba7c392e90f03e8ebdd24d53cbc08e43986d3
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-02-03 18:50:57 +0530

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,10 +1,10 @@
-n=int(input("Enter number:"))
-temp=n
+num=int(input("Enter number:"))
+temp=num
 rev=0
-while(n>0):
-    dig=n%10
+while(num>0):
+    dig=num%10
     rev=rev*10+dig
-    n=n//10
+    num=num//10
 if(temp==rev):
     print("yes")
 else:
