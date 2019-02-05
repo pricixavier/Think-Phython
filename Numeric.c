57a9df604ab972028fdfd313f5c24c256fa8874d
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-02-05 15:32:04 +0530

    Create Numeric.c

---
+++ Numeric.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main(void) {
+	char string[10];
+	int len,i;
+	scanf("%s",string);
+	len=strlen(string);
+	for(i=0;i<len;i++)
+	{
+		if(string[i]>='0' && string[i]<='9')
+		{
+			printf("yes");
+		}
+		else
+			printf("no");
+			break;
+ 
+	}
+	return 0;
+}
