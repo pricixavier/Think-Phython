f8693ef711f7159a7b6f292de5ca8c27783fdd66
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-02-11 14:13:24 +0530

    Create intervalarm.py

---
+++ intervalarm.py
@@ -0,0 +1,12 @@
+start,end=map(int,raw_input().split())
+for n in range(start+1,end ):
+   order = len(str(n))
+   sum = 0
+   temp = n
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if n == sum:
+       print(n)
