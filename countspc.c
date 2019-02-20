e9c20f88ebd9007811032b9ffaa9f25dc47baf22
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-02-20 19:57:35 +0530

    Create countspc.c

---
+++ countspc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void) 
+{
+	char ch[100];
+	int i,j,count=0;
+	scanf("%[^\n]s",ch);
+	for(j=0;ch[j]!='\0';++j)
+	{
+		
+	}
+	for(i=0;i<j;i++)
+	{
+	
+	if(ch[i]==' ')
+	{
+		count++;
+	}
+		
+	}
+	printf("%d",count);
+	
+}
