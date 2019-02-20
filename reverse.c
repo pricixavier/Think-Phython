22ee023f51c8e904d11decc7136cd83e5c9a1df2
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-02-20 22:35:14 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k,count=0;
+char a[100],b[100];
+scanf("%[^\n]s",&a);
+for(i=0;m[i]!='\0';i++)
+{
+count++;
+}
+j=0;
+for(i=count-1;i>=0;i--)
+{
+n[j]=a[i];
+j++;
+}
+for(i=0;i<count;i++)
+{
+printf("%c",b[i]);
+}
+}
