c84588d26b4a10f780b4a4e1a4c1415f353d0787
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-06-06 11:46:15 +0530

    Create proset52.py

---
+++ proset52.py
@@ -0,0 +1,12 @@
+w,b=map(int,raw_input().split())
+c,d=map(int,raw_input().split())
+e,f=map(int,raw_input().split())
+g,h=map(int,raw_input().split())
+i=d-b
+j=f-h
+k=e-c
+l=g-w
+if i==j== k==l:
+	print("yes")
+else:
+	print("no")
