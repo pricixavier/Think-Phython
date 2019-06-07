3ead7e6f62f95a8b672842abe91fa76997d22f8a
pricixavier <46535138+pricixavier@users.noreply.github.com>
2019-06-07 12:20:13 +0530

    Create beginner52.py

---
+++ beginner52.py
@@ -0,0 +1,41 @@
+b=int(input())
+
+if(b==1):
+
+  print("One")
+
+elif(b==2):
+
+  print("Two")
+
+elif(b==3):
+
+  print("Three")
+
+elif(b==4):
+
+  print("Four")
+
+elif(b==5):
+
+  print("Five")
+
+elif(b==6):
+
+  print("Six")
+
+elif(b==7):
+
+  print("Seven")
+
+elif(b==8):
+
+  print("Eight")
+
+elif(b==9):
+
+  print("Nine")
+
+elif(b==10):
+
+  print("Ten")
